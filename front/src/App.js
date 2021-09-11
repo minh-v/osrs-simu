b4e627c549916bdf1458151e0ab0f2d2f124ca79
Minh Vu <minhhv1@uci.edu>
2021-09-11 02:14:36 -0700

    changed to only query data on first render

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import Monster from "./components/Monster"
 import List from "./components/List"
-import monsterService from "./services/monsters"
+//import monsterService from "./services/monsters"
 
 //LESSON LEARNED: DO NOT ASSUME API PATTERNS IMMEDIATELY, WILL NEED TO DEAL WITH NICHE API
 
@@ -9,7 +9,7 @@ import monsterService from "./services/monsters"
 //fetch item api
 //fetch monster api
 
-import { gql, useQuery } from "@apollo/client"
+import { gql, useQuery, useLazyQuery } from "@apollo/client"
 //test
 const MONSTERS = gql`
   query {
@@ -31,9 +31,16 @@ const MONSTERS = gql`
 
 const App = () => {
   //const [monsters, setMonsters] = useState([]) //objects of all the monsters
+  //const [loaded, setLoaded] = useState(false)
   const [selected, setSelected] = useState({})
-  const { loading, error, data } = useQuery(MONSTERS)
-  console.log(data)
+  const [getMonsters, { loading, error, data }] = useLazyQuery(MONSTERS)
+  console.log(loading)
+
+  //only want to call query on the first render
+  useEffect(() => {
+    getMonsters()
+  }, [])
+
   useEffect(() => {
     // monsterService.getAll().then((res) => setMonsters(res))
     if (loading === false && data) setSelected(data.monsters[0])
@@ -43,7 +50,7 @@ const App = () => {
   //   setSelected(monsters[0])
   // }, [monsters])
 
-  if (loading) {
+  if (loading || !data) {
     return <h1>loading...</h1>
   }
 
